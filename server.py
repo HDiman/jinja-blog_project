52311260d86918766ebbc1960ad3f31b0a62f627
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-05-15 00:11:01 +0300

    Flask + Jinja

--- server.py
+++ server.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask import render_template
+import random
+import datetime as dt
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    random_num = random.randint(0, 10)
+    current_year = dt.datetime.now().year
 
+    return render_template("index.html", num=random_num, year=current_year)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# this project is finished
