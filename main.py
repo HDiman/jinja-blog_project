94277218bb1d0a0986a9b739afc4d2f6fdaa6a41
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-05-16 01:58:37 +0300

    Blog project finished

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("index.html", all_posts=all_posts)
+
+@app.route("/blog/<num>")
+def get_blog(num):
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", all_posts=all_posts, num=int(num))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
